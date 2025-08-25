3b7168d8aa214079bdc5d9190a321ba4070cbfbe
souravrs999 <souravraveendran6@gmail.com>
2025-08-25 12:54:17 +0530

    vercel serverless fixes

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
+const compression = require('compression');
+
+const { connectDB } = require('./config/db');
+
+dotenv.config({ path: './.env', quiet: true });
+const env = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+const app = express();
+
+app.use(compression());
+if (env === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(cors());
+app.use(express.json());
+
+const v1Routes = require('./routes/v1');
+
+app.use('/api/v1', v1Routes);
+app.get('/', (req, res) => {
+  res.send('Ridemate API is running...');
+});
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    error: 'Not found',
+  });
+});
+
+app.listen(PORT, console.info(`Server running in ${env} mode on port ${PORT}`));
+
+module.exports = app;
